adb0064fc6e59f5921a3e4d57b973bb9c1ed68bd
intikz <inti.acostaylara@kzsoftworks.com>
2021-05-13 12:36:28 -0300

    T 13 form validation, submit, ngModel

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [],
+  //'./app.component.css'
 })
 export class AppComponent {
   title = 'forms';
-  topics = ['Angular','React','Vue']
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError = true;
+
+  userModel = new User(
+    'rob',
+    'rob@a.com',
+    5555555555,
+    'default',
+    'morning',
+    true
+  );
+
+  constructor(private _enrollmentService: EnrollmentService) {}
+
+  validateTopic(value) {
+    if (value == 'default') this.topicHasError = true;
+    else this.topicHasError = false;
+  }
+
+  onSubmit() {
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      (data) => console.log('success!', data),
+      (error) => console.error('Error!', error)
+    );
+  }
 }
